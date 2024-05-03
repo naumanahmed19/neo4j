a7004255773f34ab5b076de948feead022bd5e4b
Nomi <naumanahmed19@gmail.com>
2024-05-03 22:31:46 +0200

    feat: added sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="sidebar">
+        <button class="close-btn" @click="$emit('onClose')">X</button>
+        <slot />
+      
+    </div>   
+</template>
+
+<style  scoped>
+.sidebar{  
+    top:0; 
+    position: fixed;
+    height: 100vh;
+    right: 0;
+    width: 300px;
+    background: #fff;
+    padding: 2rem;
+}
+.close-btn{
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+</style>
